5d7655354319c13f41dde63f9bbe3d3e1ffd8bc7
DatGatto <traceformula2000@gmail.com>
2020-03-28 01:42:27 +0700

    Initialize Swagger

--- graphql-node-mongo-tutorial/src/index.js
+++ graphql-node-mongo-tutorial/src/index.js
@@ -8,6 +8,19 @@ const startServer = async () => {
 	//Create app
 	const app = express();
 
+	//Register swagger
+	//  1) Using expressOasGenerator to generate swagger tempate
+	const expressOasGenerator = require('express-oas-generator');
+	expressOasGenerator.init(
+		app,
+		function(spec) { return spec; },
+		'swagger.json'		
+	) // to overwrite generated specification's values use second argument.
+	//  2) Using Swagger-ui-express to show to the Web UI
+	var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('../swagger.json');
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 	//Create router
 	const router = require('express').Router();
 
