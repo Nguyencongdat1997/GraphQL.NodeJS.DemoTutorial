f6f4de00038e345fcc10c6758ece8b2fd442f389
DatGatto <traceformula2000@gmail.com>
2020-03-29 02:05:27 +0700

    Implement Schema & Resolvers

--- graphql-node-mongo-tutorial/src/schema/resolvers.js
+++ graphql-node-mongo-tutorial/src/schema/resolvers.js
@@ -1,9 +1,66 @@
 var models = require("../models/models.js");
+var mongoose = require('mongoose');
+var ObejctId = mongoose.Schema.Types.ObjectId;
 
 var resolvers = {
-  Query: {
-    hello: () => "hi"
-  }
+
+    Query: {
+        hello: () => "hi",
+        users: async (parent, args, context, info) => {
+            var users = models.User.find(function (err, users) {        
+                if (err) {
+                    return [];
+                } else {
+                    return users;
+                }
+            });      
+            return users;
+        },
+        user: async (parent, args, context, info) => {
+            var user = models.User.findOne({_id: args._id}, function (err, user) {
+                if (err) {
+                    return null;
+                } else {
+                    return user;
+                }
+            });
+            return user;
+        },
+        roles: async (parent, args, context, info) => {
+            var roles = models.Role.find(function (err, roles) {        
+                if (err) {
+                    return [];
+                } else {
+                    return roles;
+                }
+            });      
+            return roles;
+        },
+        role: async (parent, args, context, info) => {
+            return  models.Role.findOne({_id: args._id});
+        }
+    },
+
+    Mutation: {
+        createUser: async (_, { name, role }) => {
+            const newUser = new models.User({
+                                                _id: new mongoose.Types.ObjectId(),
+                                                name: {
+                                                    firstName: name.firstName,
+                                                    lastName: name.lastName,
+                                                },
+                                                role : role._id
+                                            });
+            await newUser.save();
+            return newUser;
+        }
+    },
+
+    User: {
+        role: async (parent) => {
+            return  models.Role.findOne({_id: parent.role});
+        }
+    },
 };
 
 
--- graphql-node-mongo-tutorial/src/schema/typeDefs.js
+++ graphql-node-mongo-tutorial/src/schema/typeDefs.js
@@ -1,10 +1,40 @@
 var { ApolloServer, gql } = require('apollo-server-express');
 
 var typeDefs = gql`
-  type Query {
-    hello: String!
+    type Query {
+        hello: String!
+        role(_id: ID!): Role
+        roles: [Role]!
+        user(_id: ID!): User
+        users: [User]!
     
-  }
+    }
+
+    type Mutation {
+        createUser(name: InputCreateUser_UserName!, role: InputCreateUser_Role!): User!
+    }  
+
+    type Role{
+        _id: ID!
+        title: String
+    }
+
+    type User {
+        _id: ID!
+        name: UserName!
+        role: Role!
+    }
+    type UserName{
+        firstName: String!
+        lastName: String!
+    }
+    input InputCreateUser_Role{
+        _id: ID!
+    }
+    input InputCreateUser_UserName{
+        firstName: String!
+        lastName: String!
+    }
 
 `;
 
